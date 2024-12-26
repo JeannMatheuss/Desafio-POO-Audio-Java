b59a4ee1586ae5012769f8978c1902d2e113eb34
Jean <jeanmatheus0612@gmail.com>
2024-12-26 18:12:16 -0300

    Criando os atributos de audio

--- src/br/com/alura/minhasmusicas/modelos/Audio.java
+++ src/br/com/alura/minhasmusicas/modelos/Audio.java
@@ -1,5 +1,8 @@
 package br.com.alura.minhasmusicas.modelos;
 
 public class Audio {
-    
+    private String titulo;
+    private int totalDeRepoducoes;
+    private int totalDeCurtidas;
+    private double classificacao;
 }
