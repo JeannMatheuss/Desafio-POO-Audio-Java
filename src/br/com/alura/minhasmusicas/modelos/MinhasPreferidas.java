6f39deb1e1b0a91ea63a503e51c531def733d137
Jean <jeanmatheus0612@gmail.com>
2024-12-26 18:58:38 -0300

    Instanciando a classe minhas preferidas na classe principal

--- src/br/com/alura/minhasmusicas/modelos/MinhasPreferidas.java
+++ src/br/com/alura/minhasmusicas/modelos/MinhasPreferidas.java
@@ -4,9 +4,9 @@ public class MinhasPreferidas {
     
     public void inclui(Audio audio) {
         if (audio.getClassificacao() >= 9) {
-            System.out.println(audio.getTitulo() + "é considerado sucesso absoluto");
+            System.out.println(audio.getTitulo() + " é considerado sucesso absoluto");
         } else {
-            System.out.println(audio.getTitulo() + "também é uma boa opção");
+            System.out.println(audio.getTitulo() + " também é uma boa opção");
         }
     }
 }
